9181eead7b8d81803aadf0e094facd6c68458898
Tubatiryaki <tubatiryaki25@gmail.com>
2024-07-18 15:14:48 +0300

    Menu, logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      //
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans - serif"],
+      },
+      colors: {
+        "nedion-fuchsia1": "#f5d0fe",
+        "nedion-fuchsia2": "#e879f9",
+        "nedion-fuchsia3": "#a21caf",
+        "nedion-grey": "#DDDDDD",
+        "nedion-white": "#F7F7F7",
+      },
+    },
   },
   plugins: [],
 };
